e3e5aad08fdaf2a80bf3bf4066a7c1ee3995248a
Syed Abu Baker Ali <syedabubakerali04@gmail.com>
2025-01-14 17:16:13 +0530

    pushed screenshot and dump file

---
+++ static/dump.sql
@@ -0,0 +1,29 @@
+-- create database visitly_db;
+
+-- create table customer (
+-- id int not null unique,
+-- customer_name varchar(255) not null,
+-- email varchar(255) not null,
+-- location varchar(255) not null
+
+-- )
+-- use visitly_db;
+-- insert into customer(id,customer_name,email,password)
+-- values(1,'syed','abubakar@gmail.com','12467'),(2,'ali','syed@gmail.com','abcb')
+
+-- use visitly_db;
+-- CREATE TABLE users (
+--     username VARCHAR(50) NOT NULL PRIMARY KEY,
+--     password VARCHAR(500) NOT NULL,
+--     enabled TINYINT(1) NOT NULL
+-- );
+
+-- CREATE TABLE authorities (
+--     username VARCHAR(50) NOT NULL,
+--     authority VARCHAR(50) NOT NULL,
+--     CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
+-- );
+
+-- CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
+
+
